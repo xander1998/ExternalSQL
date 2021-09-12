3acc8df3a0c327849999aa6b7c02a776a8fde4c9
Xander Harrison <xander.harrison77@gmail.com>
2021-09-12 01:16:38 -0400

    Cleaned fxmanifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,9 +4,10 @@ fx_version "adamant"
 
 game { "gta5", "rdr3" }
 
-server_script "index.js"
-server_script "server.lua"
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 
-files {
-    "config.json"
-}
+dependency "yarn"
+
+files "config.json"
+
+server_scripts { "index.js", "server.lua" }
